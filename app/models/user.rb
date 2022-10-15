54b2a84dca6697f56819e2838c5710e2427c8dbd
Anuj Gupta <anujgupta@windowslive.com>
2022-10-15 07:49:10 -0400

    complete associations (user, post, comment)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+
   validates :username, presence: true, length: { minimum: 4, maximum: 25 }
   validates :password, presence: true, length: { minimum: 8, maximum: 50 }
   validates :email, presence: true
